0defb1a05b937a1071056f90cfcad9b6ef76d6d7
srrasel <srrasel22a@gmail.com>
2025-12-04 11:52:19 +0600

    update

--- app/Filament/Resources/ResearchPublications/Tables/ResearchPublicationsTable.php
+++ app/Filament/Resources/ResearchPublications/Tables/ResearchPublicationsTable.php
@@ -6,7 +6,11 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+
+
 
 class ResearchPublicationsTable
 {
